97d63a4ac652c3fc3773e2e24b718b0c6c18f1c4
nam.np <nam.np@anlab.pro>
2021-02-24 16:27:34 +0700

    Add data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const DEFAULT_TEXTAREA = `\
+# H1
+## H2
+[links](https://www.freecodecamp.com)
+
+\`<div></div>\`
+
+\`\`\`
+function example () { 
+    return 0;
+}
+\`\`\`
+
+- item 1
+- item 2
+- item 3
+
+> Block Quotes!
+
+![VN](https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/125px-Flag_of_Vietnam.svg.png)
+
+**Viet Nam**
+`
